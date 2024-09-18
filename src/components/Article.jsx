1e4c36a8051ee85c69fcac30ee6f41ee9e1abaf3
stivan99 <stivanbozov@gmail.com>
2024-09-18 14:06:27 +0100

    Solved 5 and 6 - View individual article and comments associated with article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,15 +1,59 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function Article({ title, content, imageUrl }) {
+function Article({
+  title,
+  imageUrl,
+  body,
+  isOpenedArticle,
+  comments = [],
+  article_id,
+  author,
+  created_at = "",
+  votes = 0,
+  topic,
+}) {
+  if (!isOpenedArticle)
+    return (
+      <div className="Article">
+        <h2>{title}</h2>
+        <img
+          src={imageUrl}
+          alt="Image related to the article"
+          style={{ width: "50%" }}
+          className="article-image"
+        />
+        <p>Author:{author}</p>
+        <p>Topic:{topic}</p>
+        <Link to={`/articles/${article_id}`}>Read more</Link>
+      </div>
+    );
+
   return (
     <div className="Article">
       <h2>{title}</h2>
-      <p>{content}</p>
       <img
         src={imageUrl}
         alt="Image related to the article"
+        style={{ width: "50%" }}
         className="article-image"
       />
+      <p>Author:{author}</p>
+      <p>Topic:{topic}</p>
+      <p>Created At: {new Date(created_at).toLocaleDateString()}</p>
+      <p>Votes: {votes}</p>
+      <p>{body}</p>
+
+      <div className="comments-section">
+        <h3>Comments</h3>
+
+        {comments.map((comment, index) => (
+          <div key={index}>
+            <p>{comment.body}</p>
+            <p>By: {comment.author}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/OpenedArticle.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Article from "./Article";
+import { LoadingSpinner } from "./LoadingSpinner";
+
+function ArticleDetail() {
+  const { article_id } = useParams();
+  const [article, setArticle] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [comments, setComments] = useState([]);
+  const [isCommentsLoading, setIsCommentsLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://northcoders-news-be.onrender.com/api/articles/${article_id}`
+      )
+      .then((response) => {
+        console.log(response.data);
+
+        setArticle(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setIsLoading(false);
+      });
+  }, [article_id]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://northcoders-news-be.onrender.com/api/articles/${article_id}/comments`
+      )
+      .then((comments) => {
+        console.log(comments.data);
+        setComments(comments.data);
+        setIsCommentsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsCommentsLoading(false);
+      });
+  }, [article_id]);
+
+  if (isLoading || isCommentsLoading) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <Article
+      title={article.title}
+      topic={article.topic}
+      body={article.body}
+      imageUrl={article.article_img_url}
+      created_at={article.created_at}
+      author={article.author}
+      votes={article.votes}
+      comments={comments}
+      isOpenedArticle={true}
+    />
+  );
+}
+
+export default ArticleDetail;
